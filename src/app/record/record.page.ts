5202e6bd76ff79e2d1fa6f3df8471d954664159b
Victor Smith <me@victorsmith.dev>
2024-03-31 17:07:26 -0600

    de-couple map component

--- src/app/record/record.page.ts
+++ src/app/record/record.page.ts
@@ -11,21 +11,15 @@ import {
   IonCardHeader,
   IonCardContent,
 } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import View from 'ol/View';
-import { Feature, Map } from 'ol';
-import VectorSource from 'ol/source/Vector';
-import GeoJSON from 'ol/format/GeoJSON';
-import VectorLayer from 'ol/layer/Vector';
-import { Geometry } from 'ol/geom';
-import { Style, Stroke } from 'ol/style';
+
 import { fromLonLat } from 'ol/proj';
 import { RecordService } from './record.service';
 import { SensorsService } from '../sensors/sensors.service';
 import { Observable, Subject, takeUntil, timer } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { Position } from '@capacitor/geolocation';
+import { MapService } from '../components/map/map.service';
+import { MapComponent } from '../components/map/map.component';
 
 @Component({
   selector: 'app-record-page',
@@ -43,24 +37,39 @@ import { AsyncPipe } from '@angular/common';
     IonToolbar,
     IonTitle,
     IonContent,
-    ExploreContainerComponent,
     AsyncPipe,
+    MapComponent
   ],
 })
 export class RecordPage {
   constructor(
     private recordService: RecordService,
-    private sensorsService: SensorsService
+    public sensorsService: SensorsService,
+    private mapService: MapService
   ) {}
 
   public activityRunning: boolean = false;
   public timer$: Observable<number> | undefined;
   private stopTimer$ = new Subject<boolean>();
   public timeElapsed: number = 0;
+  public lastKnownPosition: number[] | null = null;
+
+  ngOnInit() {
+    // TODO: When should we unsubscribe from this? []
+    // a) When the component is destroyed
+    // b) When the activity is ended
+    // c) When the activity is paused?
+    this.sensorsService.positionEventEmitter.subscribe(
+      (position: GeolocationPosition) => {
+        this.mapService.addPointToMap(position);
+      }
+    );
+  }
 
   startActivity() {
     this.timer$ = timer(0, 1000).pipe(takeUntil(this.stopTimer$));
-    this.sensorsService.startMotionSensor();
+    this.sensorsService.startGpsTracking();
+    // this.sensorsService.startMotionSensor();
     this.activityRunning = true;
   }
 
@@ -72,73 +81,7 @@ export class RecordPage {
 
   endActivity() {
     this.pauseActivity();
+    this.sensorsService.stopGpsTracking();
     this.recordService.saveActivity();
-  }
-
-  fakeRunData = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          coordinates: [
-            [-114.11417790581667, 51.04647254789293],
-            [-114.11252069194907, 51.04652464154],
-            [-114.11111206016174, 51.04679813222526],
-            [-114.1104906049617, 51.046954411891505],
-            [-114.11022130770817, 51.04739720141649],
-            [-114.10868838488071, 51.047905101835994],
-            [-114.10744547448002, 51.04794417086836],
-            [-114.10582969774232, 51.047866026937044],
-            [-114.10369603488806, 51.04769672727309],
-            [-114.10323328689141, 51.04764096386552],
-          ],
-          type: 'LineString',
-        },
-      },
-    ],
-  };
-
-  map: Map | undefined;
-
-  vectorSource: VectorSource = new VectorSource({
-    features: new GeoJSON()
-      .readFeatures(this.fakeRunData, {
-        featureProjection: 'EPSG:3857',
-      })
-      .map((feature) => feature as Feature<Geometry>),
-  });
-
-  // Define the style for line features
-  lineStyle = new Style({
-    stroke: new Stroke({
-      color: '#ffcc33',
-      width: 4,
-    }),
-  });
-
-  vectorLayer = new VectorLayer({
-    source: this.vectorSource,
-    style: this.lineStyle,
-  });
-
-  calgaryCoords = [-114.11388000463317, 51.046448538027505];
-  calgaryMercator = fromLonLat(this.calgaryCoords);
-
-  ionViewDidEnter() {
-    this.map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-        this.vectorLayer,
-      ],
-      view: new View({
-        center: this.calgaryMercator,
-        zoom: 14,
-      }),
-    });
   }
 }
--- src/app/sensors/sensors.service.ts
+++ src/app/sensors/sensors.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Geolocation, Position } from '@capacitor/geolocation';
 
 @Injectable({
   providedIn: 'root',
@@ -6,6 +7,47 @@ import { Injectable } from '@angular/core';
 export class SensorsService {
   constructor() {}
 
+  private watchingGps: boolean = false;
+  public readonly positionEventEmitter = new EventEmitter<Position | null>();
+
+  // Location Permissions
+  public locationPermission: boolean = false;
+  public courseLocationPermission: boolean = false;
+
+  public checkGpsPermissions() {
+    Geolocation.checkPermissions().then((permissionsResult) => {
+      console.log('Permissions:', permissionsResult);
+    });
+  }
+
+  public requestGpsPermissions() {
+    Geolocation.requestPermissions().then((permissionsResult) => {
+      console.log('Permissions:', permissionsResult);
+    });
+  }
+
+  public async startGpsTracking() {
+    if (this.watchingGps) {
+      this.stopGpsTracking();
+      this.watchingGps = false;
+    } else {
+      this.watchingGps = true;
+      Geolocation.watchPosition(
+        { enableHighAccuracy: true },
+        (position, err) => {
+          console.log('New position:', position);
+          // Emit the position values to subscribers
+          this.positionEventEmitter.emit(position);
+        }
+      );
+    }
+  }
+
+  public async stopGpsTracking() {
+    Geolocation.clearWatch({ id: 'watch' });
+  }
+
+  // Motion Sensors
   public startMotionSensor() {
     console.log('startMotionSensor');
   }
