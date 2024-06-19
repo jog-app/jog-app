1bae32e7f40acda38853d782170637b830c5e3ba
victorsmith <victor7smith@gmail.com>
2024-06-19 00:54:57 -0600

    change url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,3 @@
 export const Constants = {
-  apiPath: 'http://ec2-54-218-172-191.us-west-2.compute.amazonaws.com/api',
-  localApiPath: 'http://localhost:5001/api',
+  apiPath: 'http://localhost:5001/api',
 };
